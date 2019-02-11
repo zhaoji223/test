694b2fae0f631630a9bb36609917de8544477caa
C7 <zhaoji@qingsongchou.com>
2019-02-11 17:42:44 +0800

    vue scoped

---
+++ test/vue/src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="hello">
+    <A class='class-a'></A>
+  </div>
+</template>
+
+<script>
+import A from './test'
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  components: {
+    A
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang='less'>
+  .hello {
+    .test {
+      color: #fff;
+    }
+  }
+</style>
---
+++ test/vue/src/components/test.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class='test'>
+        Hello, World!
+    </div>
+</template>
+
+<script>
+    export default {}
+</script>
+
+<style lang="less" scoped>
+    .test {
+        height: 100px;
+        width: 100px;
+        background: blue;
+    }
+</style>
